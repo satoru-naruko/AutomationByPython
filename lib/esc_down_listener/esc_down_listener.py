78233e5072de95eb3fe2e3306c4d2d372d38ba02
satoru-naruko <naruko505@gmail.com>
2025-08-24 18:08:55 +0900

    Bugfix/handle-esc-keydown (#3)
    * feat(gitignore): Add entries for esc_down_listener cache files
    
    * feat(autoclick): Refactor mouse exit handling and implement click sequence execution
    feat(esc_down_listener): Add EscDownListener class for keyboard event handling
    
    * refactor(esc_down_listener): Remove unused attributes and rename on_press method for clarity
    
    * refactor(autoclick): Remove redundant initialization of enter_exit_sequence in main block

---
+++ lib/esc_down_listener/esc_down_listener.py
@@ -0,0 +1,37 @@
+from pynput import keyboard
+from datetime import datetime
+
+class EscDownListener:
+    def __init__(self, on_esc_keydown_callback=None):
+        self.listener = None
+        self.pressed_esc = False
+        self.on_esc_keydown_callback = on_esc_keydown_callback
+                
+    def __enter__(self):
+        self._log_message("Listener: Entering the context")
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._log_message("Listener: Exiting the context")
+
+    def _on_press(self, key):
+        try:
+            if key == keyboard.Key.esc:
+                self._log_message("ESC key pressed. Stopping listener.")
+                self.pressed_esc = True
+                self.listener.stop()
+                if self.on_esc_keydown_callback:
+                    self._log_message("Calling the ESC keydown callback.")
+                    self.on_esc_keydown_callback()
+                    
+        except Exception as e:
+            self._log_message(f"Error: {e}")
+
+    def watch_keyboard(self):
+        self.listener = keyboard.Listener(on_press=self._on_press)
+        self.listener.start()
+        self.listener.join()
+        
+    def _log_message(self, message):
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{timestamp}] {message}")
