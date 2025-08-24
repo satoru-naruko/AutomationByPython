ab685a2a9e22323b913a9726ba0990ef956696a7
n-satoru <naruko505@gmail.com>
2025-08-24 18:01:52 +0900

    refactor(esc_down_listener): Remove unused attributes and rename on_press method for clarity

--- lib/esc_down_listener/esc_down_listener.py
+++ lib/esc_down_listener/esc_down_listener.py
@@ -3,21 +3,18 @@ from datetime import datetime
 
 class EscDownListener:
     def __init__(self, on_esc_keydown_callback=None):
-        self.x = 0
-        self.y = 0
         self.listener = None
         self.pressed_esc = False
         self.on_esc_keydown_callback = on_esc_keydown_callback
                 
     def __enter__(self):
         self._log_message("Listener: Entering the context")
-        self.listener = None
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self._log_message("Listener: Exiting the context")
 
-    def on_press(self, key):
+    def _on_press(self, key):
         try:
             if key == keyboard.Key.esc:
                 self._log_message("ESC key pressed. Stopping listener.")
@@ -31,7 +28,7 @@ class EscDownListener:
             self._log_message(f"Error: {e}")
 
     def watch_keyboard(self):
-        self.listener = keyboard.Listener(on_press=self.on_press)
+        self.listener = keyboard.Listener(on_press=self._on_press)
         self.listener.start()
         self.listener.join()
         
